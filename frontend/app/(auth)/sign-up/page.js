c93412763d2f5014d085b0932a7ec67576898249
Prosper Williams <wprosper92@gmail.com>
2025-03-21 13:18:05 +0100

    configured app colors and background. built sign up page layout and ready to begin.

--- frontend/app/(auth)/sign-up/page.js
+++ frontend/app/(auth)/sign-up/page.js
@@ -1,18 +1,17 @@
-
 export const metadata = {
-  title: "Sign Up",
-  description: "Sign Up to FreightFlow",
-  robots: {
-    index: false,
-    follow: false,
-  },
+   title: "Sign Up",
+   description: "Sign Up to FreightFlow",
+   robots: {
+      index: false,
+      follow: false,
+   },
 };
 
 export default function SignUp() {
-    return (
-      <div>
-        <h1>Sign Up</h1>
-        <p>Create a new account.</p>
+   return (
+      <div className="min-h-screen w-full flex flex-row justify-evenly" style={{border: "1px solid red"}}>
+         <section className="border hidden lg:flex flex-grow">hi</section>
+         <section className="border w-[470px] xl:w-[634.2px]">hello</section>
       </div>
-    );
-  }
+   );
+}
