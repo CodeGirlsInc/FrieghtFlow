933d7c52548a57a9c59f1b0e96426c0e987c8374
Prosper Williams <wprosper92@gmail.com>
2025-03-22 19:24:52 +0100

    completed design implementation and form validation.

--- frontend/app/(auth)/sign-up/page.js
+++ frontend/app/(auth)/sign-up/page.js
@@ -4,6 +4,9 @@ import Input from "@/components/ui/Input";
 import {FormProvider, useForm, useWatch} from "react-hook-form";
 import Button from "@/components/ui/Button";
 import Link from "next/link";
+import Image from "next/image";
+import backgroundimage from "@/public/backgroundimage.svg";
+import Select from "@/components/ui/Select";
 // export const metadata = {
 //    title: "Sign Up",
 //    description: "Sign Up to FreightFlow",
@@ -28,28 +31,29 @@ export default function SignUp() {
          setPasswordMatch(false);
       }
    };
-
+   const options = [
+      { value: '1', label: 'Blue' },
+      { value: '2', label: 'Red' },
+      { value: '3', label: 'Green' },
+    ];
    return (
-      <div className="min-h-screen w-full flex flex-row justify-evenly" style={{border: "1px solid red"}}>
-         <section className="border hidden lg:flex flex-grow relative">
-            {/* <Image src={backgroundimage} height={100} width={100} className="w-[100%] h-[100vh] max-h-[100vh] absolute top-0 bottom-0 right-0 left-0" /> */}
+      <div className="min-h-screen w-full flex flex-row justify-evenly">
+         <section className=" hidden lg:flex flex-grow relative">
+            <div className="absolute border top-0 bottom-0 left-0 right-0 bg-[#00000085]/50 z-40"></div>
+            <Image src={backgroundimage} height={100} width={100} className="w-[100vw] h-[100vh] max-h-[110vh] absolute top-0 bottom-0 right-0 left-0 z-0" alt="background image" />
          </section>
-         <section className="border w-[470px] xl:w-[634.2px] flex flex-col items-center py-16 px-8 lg:px-14 xl:px-20">
+         <section className=" w-[470px] xl:w-[634.2px] flex flex-col items-center py-16 px-8 lg:px-14 xl:px-20">
             <div className=" min-h-[547.6px] h-auto w-full space-y-5 ">
                <h1 className="text-[var(--headerText)] font-semibold text-[36px] open_sans">Welcome👋</h1>
                {/*  */}
                <FormProvider {...method}>
                   <form onSubmit={method.handleSubmit(onSubmit)} className="flex flex-col space-y-4">
                      <Input type={"text"} name={"Company_Name"} label={"Company Name"} placeholder={"Company Name"} required={true} ErrorMessage={"Company name is required"} />
-                     <Input
-                        type={"select"}
-                        name={"Business_Type"}
-                        label={"Business Type"}
-                        placeholder={"Eg Shipper, Carrier, Freight, Broker"}
-                        required={true}
-                        ErrorMessage={"please select a business type"}
-                     />
+                     {/*  */}
+                     <Select name={"Business_Type"} label={"Busines Type"} value1={"Shipper"} value2={"Carrier"} value3={"Freight Broker"}/>
+                     {/*  */}
                      <Input type={"email"} name={"Email"} label={"Email"} placeholder={"Example@gmail.com"} required={true} ErrorMessage={"Email is required!"} />
+
                      <Input
                         type={"password"}
                         name={"Password"}
@@ -73,7 +77,7 @@ export default function SignUp() {
                      <section className="flex flex-col items-center space-y-12">
                         {" "}
                         <Button type={"submit"} text={"Sign Up"} className={`w-full h-[57px] open_sans rounded-[4.5px] text-[15.6px] `} />
-                        <p className="flex gap-1 text-center">
+                        <p className="flex gap-1 text-center open-sans text-[15.6px]">
                            Already have an account ?{" "}
                            <Link href={"sign-in"} className="text-blue-600">
                               Sign In
--- frontend/components/ui/Input.jsx
+++ frontend/components/ui/Input.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import {useForm, useFormContext} from "react-hook-form";
 
-const Input = ({type, name, label, placeholder, required, ErrorMessage, minLength}) => {
+const Input = ({type, name, label, placeholder, required, ErrorMessage, minLength, list}) => {
    const {
       register,
       formState: {errors},
@@ -27,6 +27,7 @@ const Input = ({type, name, label, placeholder, required, ErrorMessage, minLengt
          <input
             type={type}
             id={name}
+            list={list}
             className={`w-full h-[57px] rounded-[4.5px] text-[#8897ad] text-[15.6px] px-4 border-[var(--border)] border-[1.3px] border-solid bg-[var(--inputBackground)] focus:outline-none open_sans font-[400]`}
             placeholder={placeholder}
             {...register(name, validationRules)}
