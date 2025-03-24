c99e1e60912bdd057234980b567d4abfb77ad400
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2025-03-24 21:40:25 +0100

    feat: built the pricing section

---
+++ frontend/components/Pricing.jsx
@@ -0,0 +1,69 @@
+import PricingCard from "./PricingCard";
+
+const pricingPlans = [
+  {
+    title: "Free Tier",
+    description: "Basic Features",
+    price: 0,
+    features: [
+      "place holder",
+      "place holder",
+      "place holder",
+      "place place holder",
+    ],
+    featured: false,
+  },
+  {
+    title: "Pay-Per-Use Model",
+    description: "For small business",
+    price: 29,
+    features: [
+      "place holder",
+      "place holder",
+      "place holder",
+      "place place holder",
+    ],
+    featured: true,
+  },
+  {
+    title: "Subscription Plans",
+    description: "Monthly/Yearly",
+    price: 49,
+    features: [
+      "place holder",
+      "place holder",
+      "place holder",
+      "place place holder",
+    ],
+    featured: false,
+  },
+  {
+    title: "Enterprise Custom Solution",
+    description: "For enterprise",
+    price: 49,
+    features: ["place holder", "place holder", "place holder"],
+    featured: false,
+  },
+];
+
+function Pricing() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#F4EBDA] py-3 space-y-5">
+      <div className="text-center space-y-3">
+        <h1 className="text-3xl font-semibold text-[#313435] uppercase">
+          Pricing Plans
+        </h1>
+        <p className="font-light text-[#0C1421] text-lg">
+          Get personalized recommendations based on your location
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row gap-6 justify-center items-center mt-3">
+        {pricingPlans.map((plan, index) => (
+          <PricingCard key={index} {...plan} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Pricing;
