7390fa831a94b8e6ecc426b8cc83dc462516437b
Michelle031 <mbaangel2803@gmail.com>
2025-03-24 19:55:28 +0100

    done with hero sections

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image"
 import Partners from "../components/Partners"
+import HomepageHero from "@/components/HomepageHero";
+import HomepageHero2 from "@/components/HomepageHero2";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
         <Image
           className="dark:invert"
@@ -49,7 +51,8 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-
+        <HomepageHero />
+        <HomepageHero2 />
         <Partners />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
