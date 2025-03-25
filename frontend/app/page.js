8f96996dc7ac96b47769bbc60f740ec54b13c357
Ayomide Adeniran <ayomideratata@gmail.com>
2025-03-25 11:00:33 -0400

    created the worldwide ui

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Partners from "../components/Partners"
+import Worldwide from "../components/Worldwide"
 
 export default function Home() {
   return (
@@ -51,6 +52,7 @@ export default function Home() {
         </div>
 
         <Partners />
+        <Worldwide />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
