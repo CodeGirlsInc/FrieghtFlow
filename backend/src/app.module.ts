7bc4a0d3b4d96a6f7617d176d03ab76b38348e9b
MaryammAli <maryamaliyu09159@gmail.com>
2025-03-28 14:10:37 +0100

    create the pricing module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/notifications.module';
+import { PricingModule } from './pricing/pricing.module';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, PricingModule],
   controllers: [AppController],
   providers: [AppService],
 })
