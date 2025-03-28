aafe47258640939ff0a0e9b0a536f99e472b4349
kamaldeen <kamalaliyu503@gmail.com>
2025-03-28 11:51:26 +0100

    Admin resource Created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/notifications.module';
+import { AdminModule } from './Admin/Admin.module';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule,AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
