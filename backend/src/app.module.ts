03c7e2ee9a21d0557f28d610dde560f9b1c3177a
Yusuf Falade <faladeyusuf54@gmail.com>
2025-04-30 05:49:10 +0100

    configured environental validation with joi and set up global validation pipes and additioal folder cleanup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,40 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/notifications.module';
 import { PricingModule } from './pricing/pricing.module';
-import { AdminModule } from './Admin/Admin.module';
 import { PaymentsModule } from './payments/payments.module';
 import { UsersModule } from './users/users.module';
 import { ShipmentsModule } from './shipments/shipments.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [NotificationsModule, PricingModule, PaymentsModule, AdminModule, UsersModule, ShipmentsModule, AuthModule],
+  imports: [
+    NotificationsModule,
+    PricingModule,
+    PaymentsModule,
+    UsersModule,
+    ShipmentsModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: configService.get('DATABASE_AUTOLOAD'),
+        synchronize: configService.get('DATABASE_SYNC'),
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/wallet/enums/transaction-type.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum TransactionType {
-  DEPOSIT = "DEPOSIT",
-  WITHDRAWAL = "WITHDRAWAL",
-}
-
