2d4314aee686dfe5e1c5363b778905d4f0b20d77
feyishola <feyisholaologunebi@gmail.com>
2025-10-01 12:44:53 +0100

    fleet feature implemented

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,6 +9,7 @@ import { ShipmentModule } from './shipment/shipment.module';
 import { InsuranceModule } from './insurance/insurance.module';
 import { CustomsComplianceModule } from './customs/customs-complaince.module';
 import { GoodsTrackingModule } from './goods-tracking/goods-tracking.module';
+import { FleetModule } from './fleet/fleet.module';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { GoodsTrackingModule } from './goods-tracking/goods-tracking.module';
   InsuranceModule,
   CustomsComplianceModule,
   GoodsTrackingModule,
+  FleetModule,
   ],
   controllers: [AppController],
   providers: [AppService],
