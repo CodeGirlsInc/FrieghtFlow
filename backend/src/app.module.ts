40cf38313b6a22d7927ef8704c2c01460f3326d1
KAMALDEEN333 <kamalaliyu503@gmail.com>
2026-01-23 10:45:02 +0000

    Created Job management system

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FreightJobsModule } from './freight-jobs/freight-jobs.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
     }),
+    FreightJobsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
